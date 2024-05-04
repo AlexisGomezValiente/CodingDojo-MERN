375fc64278f513f298cd25af0a8cee876428a3a8
Alexis Gomez <103712660+AlexisGomezValiente@users.noreply.github.com>
2024-05-04 19:38:25 -0400

    Primer React con JSX

---
+++ Stack MERN/React/Primer React con JSX/tareajsx/src/App.js
@@ -0,0 +1,13 @@
+import Tareas from './Tareas';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things i need to do:</h2>
+      <Tareas />
+    </div>
+  );
+}
+
+export default App;
---
+++ Stack MERN/React/Primer React con JSX/tareajsx/src/Tareas.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Tareas = () => {
+    return(
+        <div>
+            <ul>
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a marathon</li>
+                <li>Feed the dogs</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Tareas;
