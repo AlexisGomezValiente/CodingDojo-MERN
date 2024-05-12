505a28cfdfa23d9c2229112ff4f2677da691085b
Alexis Gomez <103712660+AlexisGomezValiente@users.noreply.github.com>
2024-05-12 12:39:30 -0400

    Poniendo todo junto

--- Stack MERN/React/apoyalo/src/PersonCard.jsx
+++ Stack MERN/React/apoyalo/src/PersonCard.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useState } from "react";
 
 const PersonCard = (props) => {
+    const [age, setAge] = useState(props.age);
+
+    const aumentar = () => {
+        setAge(age + 1);
+    }
+
     return(
         <div>
             <h1>{props.firstName}, {props.lastName}</h1>
-            <p>Age: {props.age}</p>
+            <p>Age: {age}</p>
             <p>Hair Color: {props.hairColor}</p>
+            <button onClick={aumentar}>Birthday Button for {props.firstName} {props.lastName}</button>
         </div>
     )
 }
