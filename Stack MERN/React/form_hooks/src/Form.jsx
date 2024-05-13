0536006e3756a58d744c1f21f60a45c8640d887c
Alexis Gomez <103712660+AlexisGomezValiente@users.noreply.github.com>
2024-05-12 22:31:18 -0400

    Formulario Verificacion

--- Stack MERN/React/form_hooks/src/Form.jsx
+++ Stack MERN/React/form_hooks/src/Form.jsx
@@ -9,22 +9,57 @@ const Form = () => {
     confirmPass: "",
   });
 
+  const verificar = (form, event) => {
+    const input = {
+        nombre: event.target.id
+    };
+
+    if(input.nombre == 'nombre') input.length = 3;
+    if(input.nombre == 'apellido') input.length = 3;
+    if(input.nombre == 'email') input.length = 5;
+    if(input.nombre == 'pass') input.length = 8;
+    if(input.nombre == 'confirmPass') input.length = 8;
+
+    const inputs = [];
+    for(let key in form){
+        inputs.push(key);
+    }
+
+    if(!(inputs.includes(input.nombre) && form[input.nombre].length < input.length)){
+        if(input.nombre == 'confirmPass'){
+
+        }else{
+            const err = document.getElementById(`err${input.nombre}`);
+        err.classList.add('formErr');
+        }
+    }else{
+        const err = document.getElementById(`err${input.nombre}`);
+        err.classList.remove('formErr');
+    }
+  }
+
   const handleChange = (e) => {
     const input = e.target.id;
     setForm({ ...form, [input]: e.target.value });
+    verificar({ ...form, [input]: e.target.value }, e)
   };
 
   return (
     <div className="padre">
       <form action="" onChange={handleChange} className="form">
         <input type="text" placeholder="Nombre:" id="nombre" />
+        <p id="errnombre">* Debe tener mas de 2 caracteres</p>
         <input type="text" placeholder="Apellido:" id="apellido" />
+        <p id="errapellido">* Debe tener mas de 2 caracteres</p>
         <input type="text" placeholder="Email:" id="email" />
+        <p id="erremail">* Debe tener mas de 5 caracteres</p>
         <input type="password" placeholder="Password:" id="pass" />
+        <p id="errpass">* Debe tener al menos 8 caracteres</p>
         <input type="password" placeholder="Confirm password:" id="confirmPass" />
+        <p id="errconfirmPass">* No coincide</p>
       </form>
 
-      <div>
+      <div className="data">
         <h2>Your Data</h2>
         <p>Name: {form.nombre}</p>
         <p>Apellido: {form.apellido}</p>
