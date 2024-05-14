41ba25abab8674371b661259a281e93496734d5b
Alexis Gomez <103712660+AlexisGomezValiente@users.noreply.github.com>
2024-05-14 13:55:52 -0400

    Mas formularios

--- Stack MERN/React/form_hooks/src/Form.jsx
+++ Stack MERN/React/form_hooks/src/Form.jsx
@@ -9,54 +9,76 @@ const Form = () => {
     confirmPass: "",
   });
 
-  const verificar = (form, event) => {
-    const input = {
-        nombre: event.target.id
-    };
+  const [errors, setErrors] = useState({
+    nombre: "",
+    apellido: "",
+    email: "",
+    pass: "",
+    confirmPass: "",
+  });
 
-    if(input.nombre == 'nombre') input.length = 3;
-    if(input.nombre == 'apellido') input.length = 3;
-    if(input.nombre == 'email') input.length = 5;
-    if(input.nombre == 'pass') input.length = 8;
-    if(input.nombre == 'confirmPass') input.length = 8;
-
-    const inputs = [];
-    for(let key in form){
-        inputs.push(key);
-    }
-
-    if(!(inputs.includes(input.nombre) && form[input.nombre].length < input.length)){
-        if(input.nombre == 'confirmPass'){
-
-        }else{
-            const err = document.getElementById(`err${input.nombre}`);
-        err.classList.add('formErr');
-        }
-    }else{
-        const err = document.getElementById(`err${input.nombre}`);
-        err.classList.remove('formErr');
-    }
-  }
+  const verificar = (form) => {
+    const errors = {};
+    // const input = {
+    //     nombre: event.target.id,
+    //     value: event.target.value
+    // };
+    // if(input.nombre == 'nombre') input.length = 3;
+    // if(input.nombre == 'apellido') input.length = 3;
+    // if(input.nombre == 'email') input.length = 5;
+    // if(input.nombre == 'pass') input.length = 8;
+    // if(input.nombre == 'confirmPass') input.length = 8;
+    // const inputs = [];
+    // for(let key in form){
+    //     inputs.push(key);
+    // }
+    // if(!(inputs.includes(input.nombre) && form[input.nombre].length < input.length)){
+    //     if(input.nombre == 'confirmPass' && input.value != input.){
+    //     }else{
+    //         const err = document.getElementById(`err${input.nombre}`);
+    //     err.classList.add('formErr');
+    //     }
+    // }else{
+    //     const err = document.getElementById(`err${input.nombre}`);
+    //     err.classList.remove('formErr');
+    // }
+    if(form.nombre.length < 3) errors.nombre = 'Debe tener mas de 3 caracteres';
+    else errors.nombre = 'Correcto';
+    if(form.apellido.length < 3) errors.apellido = 'Debe tener mas de 3 caracteres';
+    else errors.apellido = 'Correcto';
+    if(form.email.length < 5) errors.email = 'Debe tener mas de 5 caracteres';
+    else errors.email = 'Correcto';
+    if(form.pass.length < 8) errors.pass = 'Debe tener mas de 8 caracteres';
+    else errors.pass = 'Correcto';
+    if(!form.confirmPass.length) errors.confirmPass = '';
+    else if(form.confirmPass != form.pass) errors.confirmPass = 'No coincide';
+    else errors.confirmPass = 'Correcto';
+    return errors;
+  };
 
   const handleChange = (e) => {
     const input = e.target.id;
     setForm({ ...form, [input]: e.target.value });
-    verificar({ ...form, [input]: e.target.value }, e)
+    setErrors(verificar({ ...form, [input]: e.target.value }));
   };
 
   return (
     <div className="padre">
       <form action="" onChange={handleChange} className="form">
         <input type="text" placeholder="Nombre:" id="nombre" />
-        <p id="errnombre">* Debe tener mas de 2 caracteres</p>
+        <p id="errnombre">{errors.nombre}</p>
         <input type="text" placeholder="Apellido:" id="apellido" />
-        <p id="errapellido">* Debe tener mas de 2 caracteres</p>
+        <p id="errapellido">{errors.apellido}</p>
         <input type="text" placeholder="Email:" id="email" />
-        <p id="erremail">* Debe tener mas de 5 caracteres</p>
+        <p id="erremail">{errors.email}</p>
         <input type="password" placeholder="Password:" id="pass" />
-        <p id="errpass">* Debe tener al menos 8 caracteres</p>
-        <input type="password" placeholder="Confirm password:" id="confirmPass" />
-        <p id="errconfirmPass">* No coincide</p>
+        <p id="errpass">{errors.pass}</p>
+        <input
+          type="password"
+          placeholder="Confirm password:"
+          id="confirmPass"
+        />
+        <p id="errconfirmPass">{errors.confirmPass}</p>
       </form>
 
       <div className="data">
